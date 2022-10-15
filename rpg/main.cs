a3dadc59930fe8aad1b6c0e588d385d8b5eae3f5
Francis Lavoie <francis.lavoie79@outlook.com>
2022-10-14 21:06:09 -0400

    Mouvement player fait. prototype

--- rpg/main.cs
+++ rpg/main.cs
@@ -20,7 +20,15 @@ namespace rpg
 
         private void Main_KeyDown(object sender, KeyEventArgs e)
         {
-            player.Position += new Vector(10, 10);
+            Vector vector = new Vector(0,0);
+            switch (e.KeyCode)
+            {
+                case Keys.A: vector = new Vector(-10,   0);break;
+                case Keys.W: vector = new Vector(  0, -10);break;
+                case Keys.D: vector = new Vector( 10,   0); break;
+                case Keys.S: vector = new Vector(  0,  10); break;
+            }
+            player.Position += vector;
             game.UpdatePcbWithEntity(player, (PictureBox)Controls.Find("pcbPlayer", false)[0]);
         }
     }
